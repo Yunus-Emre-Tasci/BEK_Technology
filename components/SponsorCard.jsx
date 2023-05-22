eda5863e4462aa2ff92c82a2586bb83d3d37f5c0
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-05-23 01:27:08 +0300

    Sipariş-navbar

--- components/SponsorCard.jsx
+++ components/SponsorCard.jsx
@@ -1,27 +1,30 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const SponsorCard = ({ sponsor }) => {
 
   console.log(sponsor);
   return (
-    <div className="border border-gray-300 rounded-lg shadow-md border-1 w-[250px] h-[285px]">
-      <div className="p-2 mx-auto">
-        <Image
-          src={sponsor.url}
-          alt=""
-          height={150}
-          width={230}
-          style={{ height: "150px"}}
-        />
-        <h3 className="font-extrabold mt-2"> {sponsor.title} </h3>
-        <p className="mt-1"> {sponsor.model} </p>
-        <p className="text-[12px] font-bold mt-2 pb-2">
-          {" "}
-          Çekilişte Verilecek Ürün Sayısı {sponsor.adet} Adet{" "}
-        </p>
+    <Link href="siparis">
+      <div className="border border-gray-300 rounded-lg shadow-md border-1 w-[250px] h-[285px]">
+        <div className="p-2 mx-auto">
+          <Image
+            src={sponsor.url}
+            alt=""
+            height={150}
+            width={230}
+            style={{ height: "150px" }}
+          />
+          <h3 className="font-extrabold mt-2"> {sponsor.title} </h3>
+          <p className="mt-1"> {sponsor.model} </p>
+          <p className="text-[12px] font-bold mt-2 pb-2">
+            {" "}
+            Çekilişte Verilecek Ürün Sayısı {sponsor.adet} Adet{" "}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- pages/siparis.js
+++ pages/siparis.js
@@ -1,9 +1,56 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import {
+  AiFillPlayCircle
+} from 'react-icons/ai'
+import {
+  HiMagnifyingGlass
+} from "react-icons/hi2"
 
-const Sipariş = () => {
+const Siparis = () => {
   return (
-    <div>Sipariş</div>
+    <main className = "w-[80%] mx-auto  mt-2" >
+       <Head>
+        <title>Sipariş | BEK Technology</title>
+      </Head>
+
+      <div>
+        <Link className = 'inline-block pl-2 '
+      href = "https://bek.org.tr/kuruluslar/bek-teknoloji"
+      target = "_blank" >
+          <Image
+          width={100}
+          height={100}
+          priority = {
+            true
+          }
+           style = {
+             {
+               objectFit: "contain"
+             }
+           }
+            src = "https://bek.org.tr/files/img/2fz5fyhs.jpg"
+            alt=""
+          />
+      </Link>
+      <div className = 'flex items-center justify-between px-2 mt-4' >
+         <Link href = "/"
+        className = 'flex items-center justify-center px-3 py-2 border-2 border-gray-300 rounded-lg shadow-md' >
+        < AiFillPlayCircle/>
+        <p className='ml-2 text-sm md:text-base '>Ana Ekrana Dön</p>
+        </Link>
+        <div className='border-2 border-gray-300 shadow-md rounded-lg flex justify-between items-center w-[80%] py-2 px-3 ml-1'>
+           <input className='border-none outline-none w-[80%] bg-inherit' type="text" placeholder='Ürün ismini/numarasını yazınız' />
+           <HiMagnifyingGlass className='w-6 h-5'/>
+        </div>
+      </div>
+      </div>
+
+    </main>
+    
   )
 }
 
-export default Sipariş
+export default Siparis
